66ae712fda9bbf205b3d0a622306f550d8ac1c04
DavidOteros <a24davidos@iessanclemente.net>
2024-12-04 09:53:26 +0100

    sync

---
+++ ud03/Persona.java
@@ -0,0 +1,18 @@
+package ud03;
+
+import java.util.Date;
+
+public class Persona {
+
+    //Atributos:
+    String dni;
+    String nombre;
+    String apellido1;
+    String apellido 2;
+    String correo;
+    Integer movil;
+    Date fechNacimiento;
+
+    //Métodos
+
+}
---
+++ ud03/TestPersona.java
@@ -0,0 +1,18 @@
+package ud03.javaManual;
+
+
+public class TestPersona {
+    public static void main(String[] args) {
+        Persona julito;
+        Persona outro;
+
+        julito = new.Persona();
+
+        julito.nombre = "Julito";
+        julito.correo = "sdff@fsdff";
+        julito.toString();
+    }
+
+
+
+}
