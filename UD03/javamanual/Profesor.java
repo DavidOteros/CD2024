163f7484120f23cd1dca708b868664c695fe6254
DavidOteros <a24davidos@iessanclemente.net>
2025-01-15 09:28:57 +0100

    Sync Casiñas

---
+++ UD03/javamanual/Profesor.java
@@ -0,0 +1,16 @@
+package UD03.javamanual;
+
+import java.util.Date;
+
+public class Profesor {
+    //Atributos
+    String especialidad;
+    String cuerpo;
+    boolean sustituto;
+    Date fecOposicion;
+
+    //Constructores
+    public Profesor(){
+        
+    }
+}
