fe085dcd8cd6f9dcadd262b724cf768921712d75
DavidOteros <a24davidos@iessanclemente.net>
2025-04-02 09:06:17 +0200

    sync persoana documentada

--- 3er_Trimestre/mvn.md
+++ 3er_Trimestre/mvn.md
@@ -6,3 +6,6 @@ mvn javadoc:javadoc
 
 Toca tus clases, no utilizar sin control de versiones
 mvn javadoc:fix
+
+
+mvn javadoc:jar, te permite compartir 
---
+++ 3er_Trimestre/probas/src/main/java/com/david/Estudiante.java
@@ -0,0 +1,100 @@
+package com.david;
+
+public class Estudiante {
+
+    private String nombre;
+    private int edad;
+    private double calificacion;
+
+    /**
+     * Constructor da Clase Estudiante
+     * @param nombre Nombre de Estudiante
+     * @param edad Edad de Estudiante
+     */
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calificacion = 0.0;
+    }
+
+    /**
+     * Método para poñer a calificación
+     * @param calificacion Calificación de Estudiante
+     */
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * Métogo Get Nome
+     * @return Nombre de Estudiante
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método Set Nome
+     * @param nombre Nombre de Estudiante
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Método Get Edad 
+     * @return Edad do Estudiante
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Método Set Edad 
+     * @param edad Edad do Estudiante
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Método Get Calificación
+     * @return Devolve a Calificación do Estudiante
+     */
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * Métódo booleano para saber se aproba o Estudiante
+     * @return true ou false dependendo de se o Estudiante aprobou ou no (Calificación >= 5 --> True)
+     */
+    public boolean aprobo() {
+        return calificacion >= 5.0;
+    }
+
+    /**
+     * Método que calcula a Edade Escolar do Estudiante
+     * @return Edad Escolar do Estudiante 
+     */
+    public int edadEscolar() {
+        return edad / 12;
+    }
+
+    /**
+     * Método booleano que verifica se o Estudiante é maior de 18 anos
+     * @return Booleano que verifica se o Estudiante está na idade universitaria ou no (Edad >= 18 --> True)
+     */
+    public boolean esUniversitario() {
+        return edad >= 18;
+    }
+
+    /**
+     * Método que imprime a información do Usuario
+     */
+    public void imprimirInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Calificación: " + calificacion);
+    }
+}
